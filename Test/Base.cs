d3437465754b76d08ff678f060b8fbe8e4bf5ffe
VJ(Vijay) <34496439+vijaytestautomation@users.noreply.github.com>
2018-02-27 11:49:41 +1300

    Add files via upload

---
+++ Test/Base.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Ebay.Global;
+using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+
+namespace Ebay
+{
+    public abstract class Base
+    {
+
+        #region To access path from Resource file
+        public static int Browser = Int32.Parse(KeyResource.Browser);
+        public static string ExcelPath = KeyResource.ExcelPath;
+        public static string ScreenshotPath = KeyResource.ScreenShotPath;
+        public static string ReportPath = KeyResource.ReportPath;
+        #endregion
+
+
+        #region setup and tear down
+        [SetUp]
+        public void Initialize()
+        {
+            switch (Browser)
+            {
+                case 1:
+                    Driver.driver = new FirefoxDriver();
+                    //Maximize browser window
+                    Driver.driver.Manage().Window.Maximize();
+                    break;
+                case 2:
+                    Driver.driver = new ChromeDriver();
+                    //Maximize browser window
+                    Driver.driver.Manage().Window.Maximize();
+                    break;
+            }
+            if (KeyResource.IsLogin == "true")
+            {
+                Login loginobj = new Login();
+                loginobj.LoginSuccessfull();
+
+            }
+            else
+            {
+              //Register Module
+            }
+            
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            //Screenshot
+            String img = SaveScreenShotClass.Capture(Driver.driver, "Report");
+            Driver.driver.Close();
+        }
+        #endregion
+    }
+}
---
+++ Test/Sprint.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Ebay.Pages;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+
+namespace Ebay
+{
+    
+    public class Sprint
+    {
+        class Keys : Base
+        {
+            //Validating the signin functionality.
+            [Test]
+            public void ValidateSignin()
+            {
+                var search = new SearchPage();
+                search.ValidateSignin();
+
+            }
+
+            //Validating Error message on Signin page for Invalid Data.
+            [Test]
+            public void ValidateSigninErrormessage()
+            {
+                var search = new SearchPage();
+                search.ValidateErrorMsg();
+
+            }
+
+            //Validate Advanced Search functionality.
+            [Test]
+            public void ValidateAdvancedSearch()
+            {
+                var search = new SearchPage();
+                search.ValidateSearch();
+
+            }
+           
+        }
+    }
+}
